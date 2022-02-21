f7feb64073c17ca4a85317f59452857d2bc97b67
hey210056 <76866137+hey210056@users.noreply.github.com>
2022-02-21 21:40:41 +0900

    baekjoon_2741
    백준2741

---
+++ baekjoon2741.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class baekjoon2741 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		for(int k=1; k<=a; k++) {
+			System.out.println(k);
+		}
+		sc.close();
+	}
+
+}
